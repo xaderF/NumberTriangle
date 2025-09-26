a747957430918f75704a1c1c1a934667dc4c03d0
xaderF <ryanyu365@gmail.com>
2025-09-26 16:18:09 -0400

    retrieve function

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,7 +88,18 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        if (path.isEmpty()) {
+            return root;
+        }
+        else {
+            if (path.charAt(0) == 'l') {
+                return left.retrieve(path.substring(1));
+            }
+            else if (path.charAt(0) == 'r') {
+                return right.retrieve(path.substring(1));
+            }
+        }
+
         return -1;
     }
 
